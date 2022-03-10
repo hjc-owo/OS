1c21268af55dafceb7970fb5b682aa62d06d70f8
hjc_owo <19241027@buaa.edu.cn>
2022-03-10 21:32:14 +0800

    lab0.1

--- dst/sh_test/hello_os.sh
+++ dst/sh_test/hello_os.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-command < $2 1>$3
-sed -n '8p' $2
-sed -n '32p' $2
-sed -n '128p' $2
-sed -n '512p' $2
-sed -n '1024p' $2
+command >$2
+sed -n '8p' $1
+sed -n '32p' $1
+sed -n '128p' $1
+sed -n '512p' $1
+sed -n '1024p' $1
 
