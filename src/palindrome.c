4c38ae89f3a29fbc42f3df9238de7ba05bf9eeb4
hjc_owo <19241027@buaa.edu.cn>
2022-03-10 20:55:55 +0800

    0.1-1

--- src/palindrome.c
+++ src/palindrome.c
@@ -1,23 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+int ispalindrome(char *p, int n)
+{
+    for (int i = 0; i < n / 2; i++)
+        if (*(p + i) != *(p + n - 1 - i))
+            return 0;
+    return 1;
+}
+
 int main()
 {
-	int n;
-	scanf("%d",&n);
-
-
-
-
-
-
-
-
-
-
-
-	if(){
-		printf("Y");
-	}else{
-		printf("N");
-	}
-	return 0;
+    int n;
+    char str[10] = {};
+    scanf("%d", &n);
+    sprintf(str, "%d", n);
+    printf(ispalindrome(str, strlen(str)) ? "Y" : "N");
+    return 0;
 }
+
