9ef097b91cd7c09809e26030aaba063ea0363be3
hjc_owo <19241027@buaa.edu.cn>
2022-03-17 16:22:30 +0800

    balabala...

--- find_warnings.sh
+++ find_warnings.sh
@@ -1,6 +1,5 @@
 gcc -Wall $1 -o test 2> warning.txt
 a=$?
-
 grep "warning" warning.txt | sed "s/warning: //g" > result.txt
 
 if [ $a -eq 0 ]
@@ -8,7 +7,7 @@ then
 	i=1
 	while [ $i -le $2 ]
 	do
-		echo $i | ./text >> result.txt
+		echo $i | ./test >> result.txt
 		i=$[$i+1]
 	done
 fi
