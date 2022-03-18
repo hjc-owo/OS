3a5bef15c29fc84893f513aed86bf1392150c77e
hjc_owo <19241027@buaa.edu.cn>
2022-03-18 11:31:57 +0800

    ex1.2

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -20,9 +20,10 @@ int is_elf_format(u_char *binary)
 {
         Elf32_Ehdr *ehdr = (Elf32_Ehdr *)binary;
         if (ehdr->e_ident[EI_MAG0] == ELFMAG0 &&
-                ehdr->e_ident[EI_MAG1] == ELFMAG1 &&
-                ehdr->e_ident[EI_MAG2] == ELFMAG2 &&
-                ehdr->e_ident[EI_MAG3] == ELFMAG3) {
+            ehdr->e_ident[EI_MAG1] == ELFMAG1 &&
+            ehdr->e_ident[EI_MAG2] == ELFMAG2 &&
+            ehdr->e_ident[EI_MAG3] == ELFMAG3)
+        {
                 return 1;
         }
 
@@ -41,7 +42,7 @@ int is_elf_format(u_char *binary)
  */
 
 /*
-    Exercise 1.2. Please complete func "readelf". 
+    Exercise 1.2. Please complete func "readelf".
 */
 int readelf(u_char *binary, int size)
 {
@@ -55,18 +56,22 @@ int readelf(u_char *binary, int size)
         Elf32_Half sh_entry_count;
         Elf32_Half sh_entry_size;
 
-
         // check whether `binary` is a ELF file.
-        if (size < 4 || !is_elf_format(binary)) {
+        if (size < 4 || !is_elf_format(binary))
+        {
                 printf("not a standard elf format\n");
                 return 0;
         }
 
         // get section table addr, section header number and section header size.
+        ptr_sh_table = binary + ehdr->e_shoff;
+        sh_entry_count = ehdr->e_shnum;
+        sh_entry_size = ehdr->e_shentsize;
 
-        // for each section header, output section number and section addr. 
+        // for each section header, output section number and section addr.
         // hint: section number starts at 0.
-
+        for (Nr = 0; Nr < sh_entry_count; Nr++)
+                printf("%d:0x%x\n", Nr, ((Elf32_Shdr *)(ptr_sh_table + Nr * sh_entry_size))->sh_addr);
 
         return 0;
 }
