ec6036f2d845f2a5ad3ee39e384a5129ebe7afdc
hjc_owo <19241027@buaa.edu.cn>
2022-03-31 16:31:43 +0800

    extra

---
+++ my_cal/my_cal.c
@@ -0,0 +1,25 @@
+#include <my_cal.S>
+
+char a[1086], b[1086];
+int ans, i, n = 1;
+char x;
+
+int main()
+{
+	while((x = _my_getchar())!='\n')
+	{
+		a[i++] = x;
+	}
+	for (int j = i - 1; j >= 0; j--)
+	{
+		ans += n * a[j];
+		n *= 10;
+	}
+	for(i = 0; ans; i++)
+	{
+		b[i] = ans % 2 - '0';
+		ans /= 2;
+	}
+	for (int j = i - 1; j >= 0; j--)
+	_my_putchar(b[j]);
+}
