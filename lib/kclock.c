8627c5e9aa8c3f4821f50b541490d3f9fae5ab9c
GitLab <gitlab@git.os.buaa.edu.cn>
2022-04-22 10:14:05 +0800

    Initial lab3

---
+++ lib/kclock.c
@@ -0,0 +1,18 @@
+/* See COPYRIGHT for copyright information. */
+
+/* The Run Time Clock and other NVRAM access functions that go with it. */
+/* The run time clock is hard-wired to IRQ4. */
+
+
+#include <kclock.h>
+
+extern void set_timer();
+
+/*** exercise 3.14 ***/
+void
+kclock_init(void)
+{
+	// hint: use set_timer()
+	
+}
+
---
+++ lib/sched.c
@@ -0,0 +1,32 @@
+#include <env.h>
+#include <pmap.h>
+#include <printf.h>
+
+/* Overview:
+ *  Implement simple round-robin scheduling.
+ *
+ *
+ * Hints:
+ *  1. The variable which is for counting should be defined as 'static'.
+ *  2. Use variable 'env_sched_list', which is a pointer array.
+ *  3. CANNOT use `return` statement!
+ */
+/*** exercise 3.15 ***/
+void sched_yield(void)
+{
+    static int count = 0; // remaining time slices of current env
+    static int point = 0; // current env_sched_list index
+    
+    /*  hint:
+     *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
+     *     using LIST_REMOVE and LIST_INSERT_TAIL.
+     *  2. if (env_sched_list[point] is empty), point = 1 - point;
+     *     then search through `env_sched_list[point]` for a runnable env `e`, 
+     *     and set count = e->env_pri
+     *  3. count--
+     *  4. env_run()
+     *
+     *  functions or macros below may be used (not all):
+     *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
+     */
+}
