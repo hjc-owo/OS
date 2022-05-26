07a71e3b9f5a572142d4823c1bcb0e5d6755b488
hjc-owo <2535199657@qq.com>
2022-05-26 14:29:55 +0800

    lab4-2-exam-4

--- user/fork.c
+++ user/fork.c
@@ -175,26 +175,23 @@ int sfork(void) {
     return -E_INVAL;
 }
 
-#define PTE_SHARE 0x0010
-
 int make_shared(void *va) {
     int r;
     // 若 va 不在用户空间中（大于或等于 UTOP），
     // 或者当前进程的页表中已存在该虚拟页，但进程对其没有写入权限，则该函数应返回 -1 表示失败，不产生任何影响。
-    if (va >= (void *) UTOP || ((*vpt)[VPN(va)] & PTE_COW) == 0) {
+    if (va >= (void *) UTOP || ((*vpt)[VPN(va)] & PTE_R) == 0) {
         return -1;
     }
 
     // 若当前进程的页表中不存在该虚拟页，该函数应首先分配一页物理内存，并将该虚拟页映射到新分配的物理页，使当前进程能够读写该虚拟页。
     // 若无法分配新的物理页，该函数应返回 -1 表示失败。
     if ((*vpt)[VPN(va)] == 0) {
-        r = syscall_mem_alloc(0, va, PTE_V | PTE_R);
+        r = syscall_mem_alloc(0, va, PTE_V | PTE_R | PTE_LIBRARY);
         if (r < 0) {
             return -1;
         }
     }
 
     // 将当前进程中虚拟地址 va 所属的虚拟页标记为共享页，并返回其映射到的物理页的物理地址。
-    (*vpt)[VPN(va)] |= PTE_SHARE;
     return (*vpt)[VPN(va)] & 0xfffff000;
 }
