e9e36dcdfdc136ebcf7589fd4a608ddbadd952ad
hjc-owo <2535199657@qq.com>
2022-05-26 15:27:13 +0800

    lab4-2-exam-6

--- user/fork.c
+++ user/fork.c
@@ -177,22 +177,19 @@ int sfork(void) {
 
 int make_shared(void *va) {
     int r;
+    if (va >= (void *) UTOP)
+        return -1;
 
-    // 若当前进程的页表中不存在该虚拟页，该函数应首先分配一页物理内存，并将该虚拟页映射到新分配的物理页，使当前进程能够读写该虚拟页。
-    // 若无法分配新的物理页，该函数应返回 -1 表示失败。
-    if ((*vpt)[VPN(va)] == 0) {
-        r = syscall_mem_alloc(0, va, PTE_V | PTE_R | PTE_LIBRARY);
-        if (r < 0) {
+    if ((perm & PTE_V) == 0 || ((*vpd)[PDX(va)] & PTE_V) == 0) {
+        if ((r = syscall_mem_alloc(syscall_getenvid(), ROUNDDOWN(va, BY2PG), PTE_V | PTE_R | PTE_LIBRARY)) < 0)
             return -1;
-        }
+        return ROUNDDOWN((*vpt)[VPN(va)] & 0xfffff000, BY2PG);
     }
 
-    // 若 va 不在用户空间中（大于或等于 UTOP），
-    // 或者当前进程的页表中已存在该虚拟页，但进程对其没有写入权限，则该函数应返回 -1 表示失败，不产生任何影响。
-    if (va >= (void *) UTOP || ((*vpt)[VPN(va)] & PTE_R) == 0) {
+    if (((*vpt)[VPN(va)] & 0xfff & PTE_R) == 0) {
         return -1;
     }
 
-    // 将当前进程中虚拟地址 va 所属的虚拟页标记为共享页，并返回其映射到的物理页的物理地址。
-    return (*vpt)[VPN(va)] & 0xfffff000;
+    (*vpt)[VPN(va)] |= PTE_LIBRARY;
+    return ROUNDDOWN((*vpt)[VPN(va)] & 0xfffff000, BY2PG);
 }
